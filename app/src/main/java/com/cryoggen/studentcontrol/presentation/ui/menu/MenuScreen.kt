aa10bf70025e947f80b516b502673fde06432612
Sergey-Svetlov <cryoggen@gmail.com>
2022-06-24 15:35:57 +0300

    Add animate in menu

--- app/src/main/java/com/cryoggen/studentcontrol/presentation/ui/menu/MenuScreen.kt
+++ app/src/main/java/com/cryoggen/studentcontrol/presentation/ui/menu/MenuScreen.kt
@@ -1,24 +1,29 @@
 package com.cryoggen.studentcontrol.presentation.ui.menu
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.cryoggen.studentcontrol.R
-import com.cryoggen.studentcontrol.presentation.ui.list.ScreenState
 import com.cryoggen.studentcontrol.presentation.ui.theme.Shapes
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 enum class MenuType {
     TASKS, STUDENTS, ARE_YOU_SURE
 }
 
 
+@SuppressLint("CoroutineCreationDuringComposition")
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun MenuScreen(
     menuClose: () -> Unit, menuType: MenuType, onClickIconLeft: () -> Unit = {},
@@ -27,6 +32,12 @@ fun MenuScreen(
 ) {
 
     var titleText = ""
+    var menuShown by remember { mutableStateOf(false) }
+    var stateMenu by remember { mutableStateOf(true) }
+    var visibleMenu by remember { mutableStateOf(true) }
+
+    val coroutineScope = rememberCoroutineScope()
+
     titleText = when (menuType) {
         MenuType.TASKS -> {
             stringResource(id = R.string.menu_tasks)
@@ -38,37 +49,84 @@ fun MenuScreen(
             stringResource(id = R.string.menu_are_you_sure)
         }
     }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colors.onSecondary)
-            .clickable(onClick = menuClose)
-    ) {
+
+    if (stateMenu) {
+        coroutineScope.launch {
+            menuShown = true
+        }
+        stateMenu = false
+    }
+
+    if (visibleMenu) {
         Column(
             modifier = Modifier
-                .padding(8.dp)
-                .clip(shape = Shapes.large)
-                .align(alignment = Alignment.Center)
-                .background(color = MaterialTheme.colors.onSurface)
-                .clickable(enabled = false, onClick = {})
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.onSecondary)
+                .clickable(onClick = {
+                    coroutineScope.launch {
+                        menuShown = false
+                        delay(300)
+                        visibleMenu = false
+                        menuClose()
+                    }
+                }),
+            verticalArrangement = Arrangement.Center
         ) {
-            Column(
-                modifier = Modifier
-                    .padding(bottom = 8.dp, top = 16.dp)
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-
+            AnimatedVisibility(
+                visible = menuShown,
+                enter = expandHorizontally(),
+                exit = shrinkHorizontally()
             ) {
-                MenuTitleText(titleText = titleText)
-                IconButtons(
-                    menuType = menuType,
-                    onClickIconLeft = onClickIconLeft,
-                    onClickIconCenter = onClickIconCenter,
-                    onClickIconRight = onClickIconRight
-                )
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clip(shape = Shapes.large)
+                        .background(color = MaterialTheme.colors.onSurface)
+                        .clickable(enabled = false, onClick = {}),
+
+                    ) {
+
+                    Column(
+                        modifier = Modifier
+                            .padding(bottom = 8.dp, top = 16.dp)
+                            .fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+
+                        ) {
+                        MenuTitleText(titleText = titleText)
+                        IconButtons(
+                            menuType = menuType,
+                            onClickIconLeft = {
+                                coroutineScope.launch {
+                                    menuShown = false
+                                    delay(250)
+                                    visibleMenu = false
+                                    onClickIconLeft()
+                                }
+                            },
+                            onClickIconCenter = {
+                                coroutineScope.launch {
+                                    menuShown = false
+                                    delay(250)
+                                    visibleMenu = false
+                                    onClickIconCenter()
+                                }
+                            },
+                            onClickIconRight = {
+                                coroutineScope.launch {
+                                    menuShown = false
+                                    delay(250)
+                                    visibleMenu = false
+                                    onClickIconRight()
+                                }
+                            }
+                        )
+                    }
+                }
             }
         }
     }
+
 }
 
 
